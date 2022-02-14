a53372e3e8d21a7ccb52fb141552495e6727629e
rcarvalho92 <ruipedrocarvalho21@gmail.com>
2022-02-14 16:01:55 +0000

    Add Backend

---
+++ backend/StockProject/StockProject/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockProject.Controllers
+{
+    [ApiController]
+    [Route("api/stock")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        
+        
+        [HttpGet]
+        public IActionResult getProducts()
+        {
+            return Ok(ProductDataStore.Current.Products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getProduct(int id)
+        {
+
+            var product = ProductDataStore.Current.Products.Where(p => p.Id == id).FirstOrDefault();
+
+
+            return Ok(product);
+        }
+    }
+}
---
+++ backend/StockProject/StockProject/ProductDataStore.cs
@@ -0,0 +1,73 @@
+﻿using StockProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockProject
+{
+    public class ProductDataStore
+    {
+        public static ProductDataStore Current { get; } = new ProductDataStore();
+
+        public List<ProductDto> Products { get; set; }
+
+        public ProductDataStore()
+        {
+            
+            Products = new List<ProductDto>()
+            {
+                new ProductDto()
+                {
+                     Id = 1,
+                     Name = "Coat",
+                     Colour = "Blue",
+                     Description = "New collection",
+                     Brand = "Adidas",
+                     Price = 100,
+                     Quantity = 1000
+                    
+                     
+                },
+                new ProductDto()
+                {
+                    Id = 2,
+                     Name = "Shoes",
+                     Colour = "Red",
+                     Description = "New collection",
+                     Brand = "Nike",
+                     Price = 50,
+                     Quantity = 1000
+
+
+                },
+                new ProductDto()
+                {
+                    Id = 3,
+                     Name = "T-shirt",
+                     Colour = "Black",
+                     Description = "New collection",
+                     Brand = "Puma",
+                     Price = 70,
+                     Quantity = 1000
+
+                },
+                new ProductDto()
+                {
+                    Id = 4,
+                     Name = "Hat",
+                     Colour = "White",
+                     Description = "New collection",
+                     Brand = "Channel",
+                     Price = 80,
+                     Quantity = 1000
+                }
+
+            };
+        }
+
+    }
+
+}
+    
+
