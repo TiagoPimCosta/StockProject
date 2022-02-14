50912f40cb25d3f6aca58eb0567cc9cd39dfc88c
rcarvalho92 <ruipedrocarvalho21@gmail.com>
2022-02-14 16:41:26 +0000

    FixBackend

---
+++ backend/StockProject/StockProject/Business/IBusiness.cs
@@ -0,0 +1,16 @@
+﻿using StockProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockProject.Business
+{
+    public interface IBusiness
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(string name);
+
+
+    }
+}
